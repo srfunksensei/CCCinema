6c5b96cd4992f2a2a042a5e822a1a507cabe7134
srfunksensei <sr.funk.sensei@gmail.com>
2021-10-29 12:41:55 +0200

    added default implementation for mapping list of resources to MapStructConverter

--- src/main/java/com/mb/converter/mapstruct/MapStructConverter.java
+++ src/main/java/com/mb/converter/mapstruct/MapStructConverter.java
@@ -3,11 +3,21 @@ package com.mb.converter.mapstruct;
 import org.mapstruct.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper
 public interface MapStructConverter<S, T> {
 
     T toDto(final S source);
 
-    List<T> toDto(final List<S> source);
+    default List<T> toDto(final List<S> source) {
+        if (source == null) {
+            return null;
+        }
+
+        return source
+                .stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/mb/converter/mapstruct/ScreeningMapStructConverter.java
+++ src/main/java/com/mb/converter/mapstruct/ScreeningMapStructConverter.java
@@ -3,9 +3,6 @@ package com.mb.converter.mapstruct;
 import com.mb.dto.ScreeningDto;
 import com.mb.models.Screening;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class ScreeningMapStructConverter implements MapStructConverter<Screening, ScreeningDto> {
 
     @Override
@@ -23,17 +20,5 @@ public class ScreeningMapStructConverter implements MapStructConverter<Screening
                 .screeningId(source.getId())
                 .start(source.getStart())
                 .build();
-    }
-
-    @Override
-    public List<ScreeningDto> toDto(final List<Screening> source) {
-        if (source == null) {
-            return null;
-        }
-
-        return source
-                .stream()
-                .map(this::toDto)
-                .collect(Collectors.toList());
     }
 }

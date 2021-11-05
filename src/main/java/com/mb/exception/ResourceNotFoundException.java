be78d6ecf4431e505a6494638dcfe48b8c82c3c9
srfunksensei <sr.funk.sensei@gmail.com>
2021-11-05 15:54:58 +0100

    added ResourceNotFoundException

---
+++ src/main/java/com/mb/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.mb.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+@NoArgsConstructor
+public class ResourceNotFoundException extends RuntimeException {
+}

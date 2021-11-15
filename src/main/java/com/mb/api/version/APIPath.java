ccd937781db86062b7ad253d2baeadb6ddc7a246
srfunksensei <sr.funk.sensei@gmail.com>
2021-11-15 08:54:28 +0100

    added support for versioning

---
+++ src/main/java/com/mb/api/version/APIPath.java
@@ -0,0 +1,13 @@
+package com.mb.api.version;
+
+public final class APIPath {
+
+    private static final String PATH_SEPARATOR = "/";
+
+    public static final String API = PATH_SEPARATOR + "api";
+    public static final String VERSION_1 = PATH_SEPARATOR + "v1";
+    public static final String VERSION_2 = PATH_SEPARATOR + "v2";
+
+    public static final String SCREENING = PATH_SEPARATOR + "screening";
+
+}
